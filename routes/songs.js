bed0177f4ac05983488ad5bf35c56fff93d376a9
Golan <golanm16@gmail.com>
2022-01-16 17:22:16 +0200

    added song route

--- routes/songs.js
+++ routes/songs.js
@@ -1,2 +1,9 @@
 const express = require("express");
+const { appendFile } = require("fs");
 const router = express.Router();
+router.get("/getAll", (req, res) => {
+  console.log("songs");
+  express.send("songs");
+});
+
+module.exports = router;
