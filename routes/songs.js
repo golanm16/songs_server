13833df8c90cb0ee9b58321916094bb111f8f1e5
Golan <golanm16@gmail.com>
2022-01-16 17:51:44 +0200

    added get all songs route

--- routes/songs.js
+++ routes/songs.js
@@ -1,17 +1,24 @@
 const express = require("express");
 const { appendFile } = require("fs");
 const router = express.Router();
+const Song = require("../models/song.js");
+
 router.get("/", (req, res) => {
   console.log("songs 1");
   res.send("songs 1");
 });
-router.get("/songs2", (req, res) => {
+router.get("/songs2", async (req, res) => {
   console.log("songs 2");
   res.send("songs 2");
 });
-router.post("/", (req, res) => {
+router.post("/", async (req, res) => {
   console.log(req.body);
-  res.send("arrived");
+  let newSong = await new Song({ ...req.body }).save();
+  res.send(newSong);
+});
+router.get("/", async (req, res) => {
+  let songsList = await Song.find({});
+  res.send(songsList);
 });
 
 module.exports = router;
