cb5dc6fbe0ac2431cb705a724abf16a2c946ef4c
Golan <golanm16@gmail.com>
2022-01-16 17:18:10 +0200

    added routes directory

--- models/index.js
+++ models/index.js
@@ -1,11 +1,14 @@
 const mongoose = require("mongoose");
+const { Song } = require("./song.js");
+require("dotenv").config();
 
 const connectDB = async () => {
   const mongoUrl = process.env.MONGO_URL;
+  console.log(mongoUrl);
   return await mongoose.connect(mongoUrl, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
   });
 };
-
+const models = { Song };
 module.exports = { connectDB };
--- models/song.js
+++ models/song.js
@@ -1,8 +1,14 @@
 const mongoose = require("mongoose");
 const songSchema = new mongoose.Schema({
-  title: { type: string, required: true },
-  artist: { type: string, required: true },
-  src: { type: string, required: true },
-  createdBy: { type: mongoose.SchemaType.ObjectId, ref: "user" },
-  provider: { type: string },
+  title: { type: String, required: true },
+  artist: { type: String, required: true },
+  src: { type: String, required: true },
+  createdBy: {
+    type: mongoose.SchemaTypes.ObjectId,
+    ref: "user",
+    required: true,
+  },
+  provider: { type: String },
 });
+const Song = mongoose.model("Song", songSchema);
+module.exports = Song;
