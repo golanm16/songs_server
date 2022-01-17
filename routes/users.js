6394e56248b6b37dab3baa51d1dc2b23662c2b8c
Golan <golanm16@gmail.com>
2022-01-17 07:26:29 +0200

    brain melt with encryption and web access token

--- routes/users.js
+++ routes/users.js
@@ -1,8 +1,8 @@
 const express = require("express");
-const { appendFile } = require("fs");
 const bcrypt = require("bcrypt");
 const router = express.Router();
 const User = require("../models/User.js");
+const jwt = require("jsonwebtoken");
 
 router.post("/register", async (req, res) => {
   try {
@@ -14,6 +14,25 @@ router.post("/register", async (req, res) => {
     const savedUser = await user.save();
     console.log("new user saved");
     res.json(savedUser);
+  } catch (e) {
+    console.log(e);
+    res.status(500).json({ message: "internal server error" });
+  }
+});
+
+router.post("/login", async (req, res) => {
+  try {
+    const user = await User.findOne({ username: req.body.username });
+    const match = await bcrypt.compare(req.body.password, user.password);
+    if (match) {
+      const accessToken = jwt.sign(
+        JSON.stringify(user),
+        process.env.TOKEN_SECRET
+      );
+      res.json(accessToken);
+    } else {
+      res.status(400).json({ message: "invalid credentials" });
+    }
   } catch (e) {
     console.log(e);
     res.status(500).json({ message: "internal server error" });
--- server.js
+++ server.js
@@ -4,12 +4,29 @@ const bodyParser = require("body-parser");
 const cors = require("cors");
 const { connectDB } = require("./models/index.js");
 const { songsRoute, usersRoute } = require("./routes/router.js");
+const jwt = require("jsonwebtoken");
 
 const app = express();
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.json());
 app.use(cors());
+
+const authJWT = (req, res, next) => {
+  const authHeader = req.headers.authorization;
+  if (authHeader) {
+    const token = authHeader.split("")[1];
+    jwt.verify(token, process.env.TOKEN_SECRET, (err, user) => {
+      if (err) {
+        return res.sendStatus(403);
+      }
+      req.user = user;
+      next();
+    });
+  } else {
+    return res.sendStatus(401);
+  }
+};
 
 const port = process.env.PORT || 1251;
 app.listen(port, () => {
